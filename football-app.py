0a307b99ed6f8337abfd820bd806d371893719ec
thedon122 <donovan122@yahoo.com>
2021-01-19 23:08:28 -0500

    title and description

--- football-app.py
+++ football-app.py
@@ -4,3 +4,11 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
+st.title('NFL Football Stats (Rushing) Explorer')
+
+st.markdown("""
+This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
+""")
