2985a4407224595a338626696f2bdfed29078037
thedon122 <donovan122@yahoo.com>
2021-01-26 23:57:25 -0500

    Downloads player stats

--- football-app.py
+++ football-app.py
@@ -39,3 +39,13 @@ df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playersta
 st.header('Display Player Stats of Selected Team(s)')
 st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 st.dataframe(df_selected_team)
+
+# Download NBA player stats data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
